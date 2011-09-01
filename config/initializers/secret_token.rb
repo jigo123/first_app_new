ba2caf48b746cbc14fef65323825566666e93169
jigo D <dev5@openxcell.info>
2011-09-01 17:50:23 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'cba332a30105998942b463c5f0df485ba51e8d239733c6589729f9981049bedb1f5088d11d05874226ea48b321efffbb0ec572b57af54ad49255b2cbb9629cb2'
